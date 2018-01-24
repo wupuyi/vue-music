da36adef44df1f624d83a718bd848894f98587bc
Web前端-吴溥毅 <wupuyi@foxmail.com>
2018-01-24 17:36:21 +0800

    singer detail

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Recommend from 'components/recommend/recommend'
 import Singer from 'components/singer/singer'
 import Rank from 'components/rank/rank'
 import Search from 'components/search/search'
+import SingerDetail from 'components/singer-detail/singer-detail'
 
 Vue.use(Router)
 
@@ -19,7 +20,14 @@ export default new Router({
     },
     {
       path: '/singer',
-      component: Singer
+      component: Singer,
+      children: [
+        {
+          // :id 表示以id为变量，可以传入不同的id，可以渲染不同的歌手详情页
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
     },
     {
       path: '/rank',
