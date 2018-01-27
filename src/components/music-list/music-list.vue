90d086dc5ca1937687c555478136cf735e680698
Web前端-吴溥毅 <wupuyi@foxmail.com>
2018-01-27 17:24:02 +0800

    songs list end

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -18,6 +18,9 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
@@ -25,6 +28,7 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import Loading from 'base/loading/loading'
 import {prefixStyle} from 'common/js/dom'
 
 const RESERVED_HEIGHT = 40
@@ -107,7 +111,8 @@ export default {
   },
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   }
 }
 </script>
